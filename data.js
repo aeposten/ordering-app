c158bfc118fa4ce1b666fe1065e9902573c41e8c
Amy <86963506+aeposten@users.noreply.github.com>
2022-11-17 15:40:26 -0800

    Added js, css, html, imported data

---
+++ data.js
@@ -0,0 +1,25 @@
+const productArray = [
+  {
+    id: 0,
+    name: "Custom Pattern",
+    description:
+      "A custom embroidery pattern based on the image or message of your choice.",
+    images: ["./img/plant-mom.jpg", "./img/strongwomen.jpg"],
+    price: 50,
+  },
+  {
+    id: 1,
+    name: "Pet Portrait",
+    description: "An Embroidered pet portrait based on a photo you provide",
+    images: ["./img/rue.jpg", "./img/goose.jpg"],
+    price: 80,
+  },
+  {
+    id: 2,
+    name: "MTO Embroidery",
+    description:
+      "A piece of embroidery based on one of my patterns or the pattern of your choice.",
+    images: ["./img/revenge.jpg", "./img/grief.jpg"],
+    price: 75,
+  },
+];
---
+++ script.js
@@ -0,0 +1,3 @@
+import {productArray} from './data';
+
+console.log(productArray)
