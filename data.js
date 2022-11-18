ea0c850c7a36ce3d6570631864ea20ea904b6f99
Amy <86963506+aeposten@users.noreply.github.com>
2022-11-18 12:24:33 -0800

    Can add and remove items from cart

--- data.js
+++ data.js
@@ -5,12 +5,14 @@ export const productArray = [
       "A custom embroidery pattern based on the image or message of your choice.",
     images: ["img/plant-mom.jpg", "img/strong-women.jpg"],
     price: 50,
+    inCart: false,
   },
   {
     name: "Pet Portrait",
     description: "An Embroidered pet portrait based on a photo you provide",
     images: ["img/rue.jpg", "img/goose.jpg"],
     price: 80,
+    inCart: false,
   },
   {
     name: "Made to Order Embroidery",
@@ -18,5 +20,6 @@ export const productArray = [
       "A piece of embroidery based on one of my patterns or the pattern of your choice.",
     images: ["img/revenge.jpg", "img/grief.jpg"],
     price: 75,
+    inCart: false,
   },
 ];
--- script.js
+++ script.js
@@ -3,13 +3,17 @@ import { productArray } from "./data.js";
 const PRODUCTS = productArray;
 const productsSection = document.getElementById("products");
 const orderedProducts = document.getElementById("ordered-products");
-const totalPrice = document.getElementById("total-price")
+const totalPrice = document.getElementById("total-price");
 const productCart = [];
 
 renderProductHTML();
 
 document.addEventListener("click", function (e) {
-  addToCart(e.target.dataset.add);
+  if (e.target.dataset.add) {
+    addToCart(e.target.dataset.add);
+  } else if (e.target.dataset.remove) {
+    removeFromCart(e.target.dataset.remove);
+  }
 });
 
 function renderProductHTML() {
@@ -30,9 +34,10 @@ function renderProductHTML() {
 }
 
 function renderCart() {
-  const cartDivs = productCart.map(({ product, name, price }, index) => 
-    `<div class="product">
-    <span>${name}</span> <button>remove</button> <span>${price}</span>
+  const cartDivs = productCart.map(
+    ({ product, name, price }, index) =>
+      `<div class="product">
+    <span>${name}</span> <button data-remove="${index}">remove</button> <span>${price}</span>
   </div>`
   );
 
@@ -41,16 +46,24 @@ function renderCart() {
 
 function calculateTotal() {
   let total = 0;
-  productCart.forEach(({product, price}) => {
-    total += price
-  }
-  )
+  productCart.forEach(({ product, price }) => {
+    total += price;
+  });
   totalPrice.textContent = total;
 }
 
 function addToCart(productIndex) {
+  PRODUCTS[productIndex].inCart = true;
   productCart.push(PRODUCTS[productIndex]);
-  console.log(productCart);
+
   renderCart();
   calculateTotal();
+}
+
+function removeFromCart(productIndex) {
+  productCart[productIndex].inCart = false;
+  productCart.splice(productIndex, 1);
+
+  calculateTotal();
+  renderCart();
 }
