4788381e31a1d054582f8cd7fe36700f2716e8e2
Amy <86963506+aeposten@users.noreply.github.com>
2022-11-17 16:50:24 -0800

    Can render images on page

--- data.js
+++ data.js
@@ -1,25 +1,22 @@
-const productArray = [
+export const productArray = [
   {
-    id: 0,
     name: "Custom Pattern",
     description:
       "A custom embroidery pattern based on the image or message of your choice.",
-    images: ["./img/plant-mom.jpg", "./img/strongwomen.jpg"],
+    images: ["img/plant-mom.jpg", "img/strong-women.jpg"],
     price: 50,
   },
   {
-    id: 1,
     name: "Pet Portrait",
     description: "An Embroidered pet portrait based on a photo you provide",
-    images: ["./img/rue.jpg", "./img/goose.jpg"],
+    images: ["img/rue.jpg", "img/goose.jpg"],
     price: 80,
   },
   {
-    id: 2,
-    name: "MTO Embroidery",
+    name: "Made to Order Embroidery",
     description:
       "A piece of embroidery based on one of my patterns or the pattern of your choice.",
-    images: ["./img/revenge.jpg", "./img/grief.jpg"],
+    images: ["img/revenge.jpg", "img/grief.jpg"],
     price: 75,
   },
 ];
--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-import {productArray} from './data';
+import { productArray } from "./data.js";
 
-console.log(productArray)
+const productsSection = document.getElementById("products");
+
+renderProductHTML();
+function renderProductHTML() {
+
+    function renderImageHTML(images) {
+           return  images.map((image) => 
+                `<img src=${image} />`
+            ).join("")
+    }
+  const productCards = productArray.map(({product, name, images, description, price}, index) => 
+    `<div id="product-${index}" class="product">
+        <div class="product-images" id="img-${index}">${renderImageHTML(images)}</div>
+        <div class="product-info"><h3>${
+          name
+        }</h3> <div class="description">${
+      description
+    }</div><div class="price">${price}</div> </div>
+        <div class="add-to-cart" id="add-to-cart"><button>Add</button></div>
+    </div>`
+  );
+  return (productsSection.innerHTML = productCards.join(""));
+}
+
+
+
+
+console.log(productArray);
+im
